ae03ffb5d4bcf81a20da57a5f2b0d89ccdfa8127
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2023-01-14 12:41:57 +0700

    chore: go fmt

--- examples/three/main.go
+++ examples/three/main.go
@@ -9,19 +9,19 @@ import (
 )
 
 func main() {
-    scraper := twitterscraper.New()
-    i := 0
-    for tweet := range scraper.SearchTweets(context.Background(),
-        "twitter scraper data -filter:retweets", 60) {
-        if tweet.Error != nil {
-            panic(tweet.Error)
-        }
-        j, err := json.MarshalIndent(tweet, "", "  ")
-        if err != nil {
-            panic(err)
-        }
-        i++
-        fmt.Println(i)
-        fmt.Println(string(j))
-    }
+	scraper := twitterscraper.New()
+	i := 0
+	for tweet := range scraper.SearchTweets(context.Background(),
+		"twitter scraper data -filter:retweets", 60) {
+		if tweet.Error != nil {
+			panic(tweet.Error)
+		}
+		j, err := json.MarshalIndent(tweet, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		i++
+		fmt.Println(i)
+		fmt.Println(string(j))
+	}
 }
--- types.go
+++ types.go
@@ -5,43 +5,43 @@ import "time"
 type (
 	// Video type.
 	Video struct {
-		ID      string	`json:"id"`
-		Preview string	`json:"preview"`
-		URL     string	`json:"url"`
+		ID      string `json:"id"`
+		Preview string `json:"preview"`
+		URL     string `json:"url"`
 	}
 
 	// Tweet type.
 	Tweet struct {
-		Hashtags         []string `json:"hashtags"`
-		HTML             string  `json:"html"`
-		ID               string  `json:"id"`
-		InReplyToStatus  *Tweet  `json:"in_reply_to_status"`
-		IsQuoted         bool	`json:"is_quoted"`
-		IsPin            bool	`json:"is_pin"`
-		IsReply          bool	`json:"is_reply"`
-		IsRetweet        bool	`json:"is_retweet"`
-		Likes            int	`json:"likes"`
-		PermanentURL     string	`json:"permanent_url"`
-		Photos           []string	`json:"photos"`
-		Place            *Place		`json:"place"`
-		QuotedStatus     *Tweet	`json:"quoted_status"`
-		Replies          int	`json:"replies"`
-		Retweets         int	`json:"retweets"`
-		RetweetedStatus  *Tweet	`json:"retweeted_status"`
-		Text             string	`json:"text"`
-		TimeParsed       time.Time	`json:"time_parsed"`
-		Timestamp        int64	`json:"timestamp"`
-		URLs             []string `json:"urls"`
-		UserID           string	`json:"user_id"`
-		Username         string	`json:"username"`
-		Videos           []Video	`json:"videos"`
-		SensitiveContent bool	`json:"sensitive_content"`
+		Hashtags         []string  `json:"hashtags"`
+		HTML             string    `json:"html"`
+		ID               string    `json:"id"`
+		InReplyToStatus  *Tweet    `json:"in_reply_to_status"`
+		IsQuoted         bool      `json:"is_quoted"`
+		IsPin            bool      `json:"is_pin"`
+		IsReply          bool      `json:"is_reply"`
+		IsRetweet        bool      `json:"is_retweet"`
+		Likes            int       `json:"likes"`
+		PermanentURL     string    `json:"permanent_url"`
+		Photos           []string  `json:"photos"`
+		Place            *Place    `json:"place"`
+		QuotedStatus     *Tweet    `json:"quoted_status"`
+		Replies          int       `json:"replies"`
+		Retweets         int       `json:"retweets"`
+		RetweetedStatus  *Tweet    `json:"retweeted_status"`
+		Text             string    `json:"text"`
+		TimeParsed       time.Time `json:"time_parsed"`
+		Timestamp        int64     `json:"timestamp"`
+		URLs             []string  `json:"urls"`
+		UserID           string    `json:"user_id"`
+		Username         string    `json:"username"`
+		Videos           []Video   `json:"videos"`
+		SensitiveContent bool      `json:"sensitive_content"`
 	}
 
 	// ProfileResult of scrapping.
 	ProfileResult struct {
 		Profile
-		Error error	`json:"error"`
+		Error error `json:"error"`
 	}
 
 	// TweetResult of scrapping.
